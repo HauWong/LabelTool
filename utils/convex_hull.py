17d819bbe288b82c8977199cd4d69d917b66a943
HauWong <youthwhao@gmail.com>
2021-07-05 17:35:55 +0800

    为罗盘和矢量化代码添加注释

--- utils/convex_hull.py
+++ utils/convex_hull.py
@@ -11,10 +11,10 @@ class Compass(object):
 
     def __init__(self):
 
-        self.is_corner = False
-        self.direct = (0, 1)
-        self._d_idx = 4
-        self._array = np.zeros((3, 3), dtype=np.uint8)
+        self.is_corner = False  # 是否为角点
+        self.direct = (0, 1)    # 移动方向，用于指导窗口的移动，初始为向右
+        self._d_idx = 4         # 方向索引，用于在DIRECTS里查找方向，初始为4
+        self._array = np.zeros((3, 3), dtype=np.uint8)  # 罗盘所处的窗口内容
 
     def update(self, window):
         self._array = window
@@ -28,10 +28,11 @@ class Compass(object):
 
         a = self._array
         loosen = [a[0, 0], a[0, 1], a[0, 2], a[1, 2],
-                  a[2, 2], a[2, 1], a[2, 0], a[1, 0]]
-        view_link = DoubleLinkTable(loosen)
-        view = (self._d_idx+4) % 8
-        node = view_link.get_node(view)
+                  a[2, 2], a[2, 1], a[2, 0], a[1, 0]]   # 从左上角点开始，以中心点为中心顺时针拉平窗口
+        view_link = DoubleLinkTable(loosen)             # 用一个双向链表来存储窗口内容
+        view = (self._d_idx+4) % 8                      # 当前视线，与方向索引意义相同，此刻从初始方向索引向顺时针旋转半周
+        node = view_link.get_node(view)                 # 从链表中找到当前视线所对应的节点
+        # 根据节点状态调整视线
         if node.next.value == 0:
             node = node.next
             view = (view+1) % 8
@@ -46,6 +47,7 @@ class Compass(object):
                 view = (view-1) % 8
         # todo: 添加对短尾线的识别支持
 
+        # 根据视线更新方向索引，并从DIRECTS获取方向
         if self._d_idx != view and node.value:
             self.is_corner = True
             self._d_idx = view
@@ -137,20 +139,20 @@ def array2vector(array):
 
     """
     h, w = array.shape
-    corners = []
-    cps = Compass()
-    p = [0, 0]
+    corners = []        # 存储所有角点
+    cps = Compass()     # 创建罗盘对象，用于搜索窗口移动方向
+    p = [0, 0]          # 起点，左上角点
     while p not in corners:
-        win_arr = np.zeros((3, 3), dtype='uint8')
+        win_arr = np.zeros((3, 3), dtype='uint8')   # 创建窗口，用于罗盘判断角点和搜索方向
         for i0 in range(-1, 2):
             for j0 in range(-1, 2):
                 if p[0] + i0 < 0 or p[1] + j0 < 0 or p[0] + i0 >= h or p[1] + j0 >= w:
                     continue
                 win_arr[i0 + 1, j0 + 1] = array[p[0] + i0, p[1] + j0]
-        cps.update(win_arr)
+        cps.update(win_arr)     # 罗盘根据窗口内容判断角点和更新方向
         if cps.is_corner:
             corners.append(p)
-        p = [p[0]+cps.direct[0], p[1]+cps.direct[1]]
+        p = [p[0]+cps.direct[0], p[1]+cps.direct[1]]    # 移动到下一个点
 
     return corners
 
@@ -170,18 +172,5 @@ if __name__=="__main__":
     h, w = arr.shape
 
     corners = array2vector(arr)
-    # win = Window()
-    # p = [0, 0]
-    # while p not in corners:
-    #     win_arr = np.zeros((3, 3), dtype='uint8')
-    #     for i0 in range(-1, 2):
-    #         for j0 in range(-1, 2):
-    #             if p[0] + i0 < 0 or p[1] + j0 < 0 or p[0] + i0 >= h or p[1] + j0 >= w:
-    #                 continue
-    #             win_arr[i0 + 1, j0 + 1] = arr[p[0] + i0, p[1] + j0]
-    #     win.update(win_arr)
-    #     if win.is_corner:
-    #         corners.append(p)
-    #     p = [p[0]+win.direct[0], p[1]+win.direct[1]]
 
     print(corners)
